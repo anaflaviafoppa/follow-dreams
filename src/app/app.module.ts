654183765f451497a76c91de4525a6ff346d17e6
Ana Foppa <Ana.Foppa@ctw.bmwgroup.com>
2022-06-23 22:38:20 +0100

    initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './views/pages/main/main.component';
+import { NavbarComponent } from './views/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
