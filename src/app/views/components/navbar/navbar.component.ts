cb3d787b72485d6c68637b0129c5cbd625d52c54
Ana Foppa <Ana.Foppa@ctw.bmwgroup.com>
2022-06-29 23:41:28 +0100

    styling responsive main - started navbar

--- src/app/views/components/navbar/navbar.component.ts
+++ src/app/views/components/navbar/navbar.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ana-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
+  innerWidth: number = 0;
+  isNavBarExpanded: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+  }
+
+  isSmallDevice(): boolean {
+    return this.innerWidth < 500;
+  }
+
+  expandNavBar() {
+    this.isNavBarExpanded = !this.isNavBarExpanded
   }
 
 }
